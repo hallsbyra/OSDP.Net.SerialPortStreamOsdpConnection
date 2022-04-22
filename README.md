8c708493ed1737dfa8873d085a4c52337de8b039
Fredrik <fredrik@hallsbyra.se>
2022-04-22 11:20:53 +0200

    First version.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # OSDP.Net.SerialPortStreamOsdpConnection
+
+[![NuGet](https://img.shields.io/nuget/v/OSDP.Net.SerialPortStreamOsdpConnection?style=flat)](https://www.nuget.org/packages/OSDP.Net.SerialPortStreamOsdpConnection/)
+
+A plug-in to [OSDP.Net](https://github.com/bytedreamer/OSDP.Net) with a drop-in replacement of the `SerialPortOsdpConnection` that uses [SerialPortStream](https://github.com/jcurl/RJCP.DLL.SerialPortStream) instead of the built-in `System.IO.Ports.SerialPort`.
+
+`System.IO.Ports.SerialPort` is wrought with problems. If you experience stability issues with serial ports in OSDP.Net, it might be a good idea to test this connection instead.
+
+## Getting Started
+
+Install the nuget.
+
+```
+dotnet add package OSDP.Net.SerialPortStreamOsdpConnection
+```
+
+Use it in code.
+
+```cs
+var controlPanel = new ControlPanel();
+controlPanel.StartConnection(new SerialPortStreamOsdpConnection(port, baudRate));
+```
+
---
+++ src/OSDP.Net.SerialPortStreamOsdpConnection/OSDP.Net.SerialPortStreamOsdpConnection/SerialPortStreamOsdpConnection.cs
@@ -0,0 +1,43 @@
+﻿using RJCP.IO.Ports;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OSDP.Net.Connections
+{
+    public class SerialPortStreamOsdpConnection : IOsdpConnection
+    {
+        readonly SerialPortStream serialPort;
+
+        public SerialPortStreamOsdpConnection(string portName, int baudRate)
+        {
+            serialPort = new SerialPortStream(portName, baudRate);
+        }
+
+        public int BaudRate => serialPort.BaudRate;
+
+        public bool IsOpen => serialPort.IsOpen;
+
+        public TimeSpan ReplyTimeout { get; set; } = TimeSpan.FromMilliseconds(200);
+
+        public void Close()
+        {
+            serialPort.Close();
+        }
+
+        public void Open()
+        {
+            serialPort.Open();
+        }
+
+        public async Task<int> ReadAsync(byte[] buffer, CancellationToken token)
+        {
+            return await serialPort.ReadAsync(buffer, 0, buffer.Length, token);
+        }
+
+        public async Task WriteAsync(byte[] buffer)
+        {
+            await serialPort.WriteAsync(buffer, 0, buffer.Length);                
+        }
+    }
+}
