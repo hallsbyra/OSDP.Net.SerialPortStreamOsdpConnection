76a1101763b9b20c68b53f9b9cb5c64e55bb2d09
Fredrik <fredrik@hallsbyra.se>
2022-04-26 15:42:20 +0200

    feat: Add possibility to pass ILogger to underlying SerialPortStream.

--- src/OSDP.Net.SerialPortStreamOsdpConnection/SerialPortStreamOsdpConnection.cs
+++ src/OSDP.Net.SerialPortStreamOsdpConnection/SerialPortStreamOsdpConnection.cs
@@ -1,4 +1,5 @@
-﻿using RJCP.IO.Ports;
+﻿using Microsoft.Extensions.Logging;
+using RJCP.IO.Ports;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace OSDP.Net.Connections
     public class SerialPortStreamOsdpConnection : IOsdpConnection
     {
         readonly SerialPortStream serialPort;
+        readonly ILogger logger;
 
-        public SerialPortStreamOsdpConnection(string portName, int baudRate)
+        public SerialPortStreamOsdpConnection(string portName, int baudRate, ILogger logger = null)
         {
-            serialPort = new SerialPortStream(portName, baudRate);
+            serialPort = logger == null ? new SerialPortStream() : new SerialPortStream(logger);
+            serialPort.PortName = portName;
+            serialPort.BaudRate = baudRate;
+            serialPort.ReadTimeout = 100;
+            this.logger = logger;
         }
 
         public int BaudRate => serialPort.BaudRate;
@@ -22,31 +28,38 @@ namespace OSDP.Net.Connections
 
         public void Close()
         {
+            logger?.LogDebug("SerialPortStreamOsdpConnection.Close() Port: {0}", serialPort.PortName);
             serialPort.Close();
         }
 
         public void Open()
         {
+            logger?.LogDebug("SerialPortStreamOsdpConnection.Open() Port: {0}", serialPort.PortName);
             serialPort.Open();
         }
 
         public async Task<int> ReadAsync(byte[] buffer, CancellationToken token)
         {
-            serialPort.ReadTimeout = 100;
-            while(true)
+            logger?.LogDebug("SerialPortStreamOsdpConnection.ReadAsync() Port: {0}", serialPort.PortName);
+            while (true)
             {
                 // SerialPortStream ignores the passed CancellationToken. However, the ReadTimeout parameter
                 // seems to work, so we use it in a loop to simulate real cancellation.
                 var count = await serialPort.ReadAsync(buffer, 0, buffer.Length, token);
-                if(count > 0)
-                    return count;
+                if (count > 0)
+                {
+                    logger?.LogDebug("SerialPortStreamOsdpConnection.ReadAsync() Port: {0}, Count: {1}", serialPort.PortName, count);
+                    return count;                    
+                }
+                logger?.LogDebug("SerialPortStreamOsdpConnection.ReadAsync() Port: {0} - Timed out", serialPort.PortName);
                 token.ThrowIfCancellationRequested();
             }
         }
 
         public async Task WriteAsync(byte[] buffer)
         {
-            await serialPort.WriteAsync(buffer, 0, buffer.Length);                
+            logger?.LogDebug("SerialPortStreamOsdpConnection.WriteAsync({0} bytes) Port: {1}", buffer.Length, serialPort.PortName);
+            await serialPort.WriteAsync(buffer, 0, buffer.Length);
         }
     }
 }
